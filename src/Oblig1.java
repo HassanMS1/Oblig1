9493c1c3f2a4165fbf307b4a047d94409261b39a
Hassan <jada-10@hotmail.com>
2020-09-10 18:50:51 +0200

    oppgave 2

--- src/Oblig1.java
+++ src/Oblig1.java
@@ -38,7 +38,41 @@ public class Oblig1 {
 
     ///// Oppgave 2 //////////////////////////////////////
     public static int antallUlikeSortert(int[] a) {
-        throw new UnsupportedOperationException();
+
+        for(int i=1;i<a.length;i++)
+        {
+// Unsorted pair found
+            if (a[i - 1] > a[i])
+            {
+                throw new IllegalStateException("Not sorted");
+            }
+        }
+
+        for(int i=0;i<a.length;i++)
+        {
+            if(a[i]==0)
+                return 0;
+        }
+
+
+        int res = 1;
+
+        // Pick all elements one by one
+        for (int i = 1; i < a.length; i++)
+        {
+            int j = 0;
+            for (j = 0; j < i; j++)
+                if (a[i] == a[j])
+                    break;
+
+            // If not printed earlier,
+            // then print it
+            if (i == j)
+                res++;
+        }
+        return res;
+
+
     }
 
     ///// Oppgave 3 //////////////////////////////////////
